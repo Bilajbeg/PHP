11016da462430caf03d3e051ff7fe2157df3417a
Emelin Bilajbegovic <bilajbeg@gmail.com>
2023-07-19 12:31:09 +0200

    all kind of loops working

---
+++ day_3/classwork/exercise_1/index.php
@@ -0,0 +1,34 @@
+<!-- HTML boilerplate  -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    /* for($i=1;$i<=50;$i++) {
+        echo $i . " Emelin Bilajbeg" . "<br>";
+    } */
+
+  /*   $j=0;
+    while($j<50) {
+        echo $j . " Emelin Bilajbeg" . "<br>";
+    } */
+
+    $z=1;
+    do 
+    {
+        echo $z . " Emelin Bilajbeg" . "<br>";
+        $z++;
+    }
+    while($z <=50);
+
+
+
+
+
+    ?>
+</body>
+</html>
