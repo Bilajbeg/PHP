47eca8c3946ca0202adda2a56c936981fbe428a9
Emelin Bilajbegovic <bilajbeg@gmail.com>
2023-07-19 14:40:43 +0200

    output working correctly

--- day_3/classwork/exercise_3/index.php
+++ day_3/classwork/exercise_3/index.php
@@ -7,17 +7,20 @@
 </head>
 <body>
     <?php
-            // Function to find the highest value in an array
+    // Function to find the highest value in an array
     function findHighestValue($arr) {
         $highestValue = $arr[0]; // Assume the first element is the highest
-        
+
         // Loop through the array to find the actual highest value
-        foreach ($arr as $value) {
+        foreach ($arr as $i => $value) {
+            $i = $i +1;
+            echo "$i Element  = $value, " . "<br>";
+
             if ($value > $highestValue) {
                 $highestValue = $value;
             }
         }
-        
+
         return $highestValue;
     }
 
@@ -29,9 +32,31 @@
 
     // Call the function and output the result
     $result = findHighestValue($randomNumbers);
-    echo "The highest value in the array is: " . $result;
-
-
+    echo "<h1>The highest value in the array is: " . $result . "</h1>";
     ?>
+
+    
+<!--  echo "<br><br>";
+
+  $numbers = array();
+  for ($i = 0; $i < 10; $i++) {
+    $numbers[] = rand(1, 100);
+  }
+
+  $highestValue = $numbers[0]; // Initialize with the first element
+
+  foreach ($numbers as $number) {
+    echo $number . " ";
+
+    if ($number > $highestValue) {
+      $highestValue = $number;
+    }
+  }
+
+  echo "<br>The highest value in the array is: " . $highestValue;  -->
+  
+
+
+
 </body>
 </html>
