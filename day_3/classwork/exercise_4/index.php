94925fa7cad339e362b0d887497d5c3924f79f11
Emelin Bilajbegovic <bilajbeg@gmail.com>
2023-07-19 14:51:54 +0200

    add some code,result is as required

--- day_3/classwork/exercise_4/index.php
+++ day_3/classwork/exercise_4/index.php
@@ -6,6 +6,21 @@
     <title>Document</title>
 </head>
 <body>
-    
+
+<?php
+    // Loop through integers from 1 to 100
+    for ($i = 1; $i <= 100; $i++) {
+        if ($i % 3 == 0 && $i % 5 == 0) {
+            echo "Full-Stack <br>";
+        } elseif ($i % 3 == 0) {
+            echo "Back-End <br>";
+        } elseif ($i % 5 == 0) {
+            echo "Front-End <br>";
+        } else {
+            echo $i . "<br>";
+        }
+    }
+    ?>
+
 </body>
 </html>
